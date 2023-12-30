6ee1fe637a47b34849c91ce5b9a614108f9c34b2
esraaeyad123 <47371491+esraaeyad123@users.noreply.github.com>
2023-12-30 19:04:30 +0300

    Initial commit

---
+++ lang/ar/blog.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'containing' => 'المحتوى',
+
+    'popular' => 'المشهور',
+    'latest' => 'الاحدث',
+    'oldest' => 'الاقدم',
+
+    'search' => 'البحث',
+    'search_placeholder' => 'يبحث',
+    'recommended_topics' => 'الموصى به'
+];
---
+++ lang/ar/home.php
@@ -0,0 +1,16 @@
+<?php
+
+
+return [
+
+    'featured_posts' => 'الاخبار ',
+    'latest_posts' => 'اخر الاخبار',
+    'more_posts' => 'المزيد',
+
+    'hero' => [
+        'News' => 'الاخبار',
+        'title' => 'المدونة',
+        'desc' => "الوصف",
+        'cta' => 'الفئة'
+    ]
+];
